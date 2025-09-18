db3656853f408ffc008359b33d4f75a464924ed1
yaswanthpunugoti <yaswanthpunugoti@gmail.com>
2025-09-18 17:33:00 +0530

    I

--- notes-manager/backend/server.js
+++ notes-manager/backend/server.js
@@ -5,7 +5,7 @@ const connectDB = require('./utils/connectDB');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({origin: '*'}));
 app.use(express.json());
 
 connectDB(process.env.MONGODB_URI);
@@ -14,5 +14,5 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes', require('./routes/notes'));
 app.use('/api/admin', require('./routes/admin'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
